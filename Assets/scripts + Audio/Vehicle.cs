7850d6e5015d955ceb3eb54333258e883b6a326e
Matteo  Ziff <matteoziff@Matteos-MacBook-Pro-2.local>
2016-12-05 10:22:26 -0500

    getting random speed and cars

--- Assets/scripts + Audio/Vehicle.cs
+++ Assets/scripts + Audio/Vehicle.cs
@@ -5,22 +5,23 @@ public class Vehicle : MonoBehaviour {
 
 	public float setSpeed { get; set;}
 
+	Vector3 transform_delta;
+	Vector3 origin; 
 
-	public int length = -10;
 
 	// Use this for initialization
 	void Start () {
+		origin = transform.position; 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		carMovement ();
+
 	}
 
 	void carMovement() {
 		transform.position += Vector3.left * setSpeed;
-		if (transform.position.x < length) {
-			Destroy (gameObject);
-		}
+//		transform_delta = origin - transform.position;
 	}
 }
--- Assets/scripts + Audio/VehicleSpawner.cs
+++ Assets/scripts + Audio/VehicleSpawner.cs
@@ -4,23 +4,32 @@ using System.Collections;
 public class VehicleSpawner : MonoBehaviour {
 	public Vehicle[] vehicle_prefabs;
 
-	public float lane_speed = 1.0f;
+	public float lane_speed;
 
 	private Vehicle vehicle;
 
+	public float length = -.52f;
 
 	// Use this for initialization
 	void Start () {
-		int vehicleIndex = Random.Range (0, vehicle_prefabs.Length);
-		vehicle = Instantiate<Vehicle>(vehicle_prefabs[vehicleIndex]);
-		vehicle.transform.parent = transform;
-		vehicle.transform.localPosition = new Vector3(.5f, .5f, 0);
-		vehicle.setSpeed = lane_speed;
+		CreateVehicle ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-//		vehicle.carMovement ();
+		if (vehicle.transform.localPosition.x < -0.5f) {
+			Destroy(vehicle.gameObject);
+			CreateVehicle ();
+		}
+	}
+
+	void CreateVehicle(){
+		int vehicleIndex = Random.Range (0, vehicle_prefabs.Length);
+		vehicle = Instantiate<Vehicle>(vehicle_prefabs[vehicleIndex]);
+		vehicle.transform.parent = transform;
+		vehicle.transform.localPosition = new Vector3(.5f, .5f, 0);
+		vehicle.setSpeed = Random.Range(0.7f,2.0f);
+//		
 	}
 
 }
